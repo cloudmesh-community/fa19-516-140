f0cac5c1f39c5fd746230d0c9370d5631d936efd
Mohamed Habbani <habbani@gmail.com>
2019-09-19 12:01:38 +0300

    e-cloudmesh-common-2
    Develop a program that demonstrates the use of dotdict.

---
+++ cloudmesh-exercises/e-cloudmesh-common-2.py
@@ -0,0 +1,13 @@
+#This program demonistrate the use of dotdict 
+#functions which been stored in cloudmesh.common.dotdictl 
+
+from cloudmesh.common.dotdict import dotdict
+
+#storing (key:value) fields
+data = {"Program": "OK"}
+#converting stored (key:value) in a dotict dictionary
+data = dotdict(data)
+
+# using value of dotdict as a condition
+if data.Program is "OK":
+    print("This Program is to Demonstrate DotDict!")
