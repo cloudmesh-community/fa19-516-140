14d9b90cd32ebe083e09c99ab6982eda2f338ce9
Mohamed Habbani <habbani@gmail.com>
2019-09-19 12:15:59 +0300

    Update e-cloudmesh-common-4.py

--- cloudmesh-exercises/e-cloudmesh-common-4.py
+++ cloudmesh-exercises/e-cloudmesh-common-4.py
@@ -1,9 +1,13 @@
+# fa19-516-140
 #This program demonistrate the use of Shell  
 #functions which been stored in cloudmesh.common.Shell
 
 from cloudmesh.common.Shell import Shell
 
 #assigning command ls -ltr value to i identifier
+
 i=Shell.ls('-ltr')
+
 #prinitng the result of the command 
+
 print(i)
