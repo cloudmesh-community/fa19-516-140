244393d949e0bcc3afc63e9f841f9f06cfff51bb
Mohamed Habbani <habbani@gmail.com>
2019-09-19 12:03:47 +0300

    e-cloudmesh-common-4
    Develop a program that demonstrates the use of cloudmesh.common.Shell

---
+++ cloudmesh-exercises/e-cloudmesh-common-4.py
@@ -0,0 +1,9 @@
+#This program demonistrate the use of Shell  
+#functions which been stored in cloudmesh.common.Shell
+
+from cloudmesh.common.Shell import Shell
+
+#assigning command ls -ltr value to i identifier
+i=Shell.ls('-ltr')
+#prinitng the result of the command 
+print(i)
